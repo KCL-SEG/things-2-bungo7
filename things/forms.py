68bb2014741fe1de7187fb28ad30733d16ebdfc0
bungo7 <allen.kallikadan_rapheal@kcl.ac.uk>
2022-12-09 13:42:57 +0000

    update form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,17 @@
 """Forms of the project."""
 
-# Create your forms here.
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django import forms
+
+class ThingForm(forms.Form):
+    
+    name = forms.CharField(label = 'name', max_length=35)
+    description = forms.CharField(widget=forms.Textarea(attrs={"maxlength":"120"}),)
+    quantity = forms.IntegerField(
+        label = 'quantity',
+        validators=[MinValueValidator(0),MaxValueValidator(50)]
+    )
+
+    #created_at = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M:%S'], widget=forms.DateTimeInput(format='%d/%m/%Y %H:%M:%S'))
+
+    
--- things/models.py
+++ things/models.py
@@ -2,7 +2,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 class Thing(models.Model):
-    name = models.CharField(max_length=35, unique=True)
+    name = models.CharField(max_length=35,)
     description = models.CharField(max_length=120, blank=True)
     quantity = models.IntegerField(
         validators=[MinValueValidator(0),MaxValueValidator(50)]
